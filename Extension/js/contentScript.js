55e77351dc4247debbfd6182089ac0c9011f92d2
khushbu <khushbu@lakebrains.in>
2023-04-04 12:46:52 +0530

    Icons and hover effect change

--- Extension/js/contentScript.js
+++ Extension/js/contentScript.js
@@ -19,6 +19,7 @@ pcnarr = pcnarr.split("'")[0]
 pcnarr = pcnarr.split(/\\/).join("")
 pcnarr = JSON.parse(pcnarr)
 const pcn = pcnarr.customer.pcn
+console.log(pcn);
 // Adding the PCN number to the local storage
 chrome.storage.local.set({ pcn: pcn }).then(() => { });
 
@@ -318,11 +319,11 @@ function settingTreeview() {
         let icon = document.createElement("div")
         icon.classList.add("arrow")
         if (data == true && uidata[i].querySelector("ul") != null) {
-            icon.innerHTML = `<svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeInherit plus_icon_js" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="width: 20px; height: 20px; color: rgb(0, 0, 0);"><path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z"></path></svg>`
+            icon.innerHTML = `<img src="${chrome.runtime.getURL('images/arrow-right.png')}" class="plus_icon_js"  />`
             uidata[i].append(icon);
         }
         else {
-            icon.innerHTML = `<svg class="MuiSvgIcon-root close MuiSvgIcon-fontSizeInherit" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="width: 20px; height: 20px; color: rgb(0, 0, 0);"><path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z"></path></svg>`
+            icon.innerHTML = `<img src="${chrome.runtime.getURL('images/up-arrow.png')}" class="width100"  />`
             uidata[i].append(icon);
 
         }
@@ -344,21 +345,21 @@ function settingTreeview() {
         }
 
     });
-    var folderTreeList1 = document.querySelectorAll('.folder-tree li .arrow svg');
-    folderTreeList1.forEach(function (li) {
-        if (li.firstChild.classList.contains("plus_icon_js")) {
+    // var folderTreeList1 = document.querySelectorAll('.folder-tree li .arrow svg');
+    // folderTreeList1.forEach(function (li) {
+    //     if (li.firstChild.classList.contains("plus_icon_js")) {
 
-            li.addEventListener('click', function (evt) {
-                evt.target.parentElement.parentElement.parentElement.classList.toggle('expanded');
-                if (evt.target.parentElement.parentElement.parentElement.classList.contains("expanded")) {
-                    evt.target.innerHTML = `<path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z"></path>`
-                } else {
-                    evt.target.innerHTML = `<path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z"></path>`
-                }
-            });
-        }
+    //         li.addEventListener('click', function (evt) {
+    //             evt.target.parentElement.parentElement.parentElement.classList.toggle('expanded');
+    //             if (evt.target.parentElement.parentElement.parentElement.classList.contains("expanded")) {
+    //                 evt.target.innerHTML = `<path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z"></path>`
+    //             } else {
+    //                 evt.target.innerHTML = `<path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z"></path>`
+    //             }
+    //         });
+    //     }
 
-    });
+    // });
 
     // Functionaltiy to set the data in Equipment search bar
     let livalue = document.querySelectorAll(".rowdataviewcontainer ul li span")
