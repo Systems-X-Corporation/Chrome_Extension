6b3a2f24475e126d79284a4dbf38aa50d3615266
= <support@lakebrains.in>
2023-05-05 12:58:29 +0530

    token issue fixed

--- Extension/js/popup.js
+++ Extension/js/popup.js
@@ -5,7 +5,9 @@ async function getCurrentTab() {
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
 }
-
+var input = document.getElementById("Token_input");
+var checkbox = document.getElementById("EquipmentCheck");
+let pcn
 // Creating new window for cloud.plex on click
 document.querySelector("#plexux").addEventListener("click", function () {
     window.open("https://cloud.plex.com/", '_blank').focus();
@@ -14,28 +16,38 @@ document.querySelector("#plexux").addEventListener("click", function () {
 document.querySelector("#plexclassic").addEventListener("click", function () {
     window.open("https://www.plexonline.com/", '_blank').focus();
 })
+var button = document.getElementById("tokensavebtn");  
 
+document.getElementById("Token_input").addEventListener("input", () => {
+   console.log(input.value);    
+    if (input.value.trim() !== "") {
+      button.disabled = false;
+      checkbox.disabled = false;
+      checkbox.style.cursor = "pointer"
+      button.style.backgroundColor="#2c6e3a";
+      button.style.cursor="pointer";
+      button.style.color="white";
+      button.style.border = "2px solid white"
+    } else {
+      button.disabled = true;
+      checkbox.disabled = true;
+      checkbox.style.cursor = "default"
+      button.style.backgroundColor= "#5a8e65";
+      button.style.cursor="default";
+      button.style.border = "2px solid #fa8178"
+    }
+})
 
 // Saving the token value in save btn
 document.querySelector("#tokensavebtn").addEventListener("click", async function () {
     let value = document.querySelector("#Token_input").value
     // Save the token in local storage
     chrome.storage.local.set({ TokenValue: value }).then(() => { 
-        // document.querySelector("#Token_input").value =" ";
-        // document.querySelector("#Token_input").ariaPlaceholder =" Enter value";
     });
     // Reloading the page after click
     location.reload();
     let tab = await getCurrentTab()
     chrome.tabs.update(tab.id, { url: tab.url });
-
-    // chrome.storage.local.get('inputText', function() {
-    //    document.getElementById('Token_input').value = '';
-    //   })
-    //   chrome.storage.local.remove('inputText', function() {
-    //     document.getElementById('Token_input').value = ''; 
-    //   });
-    
 })
 
 // Getting the token and placing it into popup token input 
@@ -43,9 +55,24 @@ chrome.storage.local.get(["TokenValue"]).then((result) => {
     Token = result.TokenValue
     console.log(Token);
     if (Token) {
-        document.querySelector("#Token_input").value = Token;
+        input.value = Token;
     }
-    
+    if (input.value.trim() !== "") {
+        button.disabled = false;
+        checkbox.disabled = false;
+        checkbox.style.cursor = "pointer"
+        button.style.backgroundColor="#2c6e3a";
+        button.style.cursor="pointer";
+        button.style.color="white";
+        button.style.border = "2px solid white"
+      } else {
+        button.disabled = true;
+        checkbox.disabled = true;
+        checkbox.style.cursor = "default"
+        button.style.backgroundColor= "#5a8e65";
+        button.style.cursor="default";
+        button.style.border = "2px solid #fa8178"
+      }
 })
 
 var checkbox = document.querySelector("#EquipmentCheck");
@@ -78,59 +105,36 @@ checkbox.addEventListener('change', async function () {
     }
 });
 
-// document.getElementById("Token_input").addEventListener("change", () => {
-//     let val = e.target.value
-//     if(val){
-//         document.getElementById("tokensavebtn").disabled = false
-//         document.getElementById("tokensavebtn").style.color = "#fff"
 
-//     }
-//     else {
-//         document.getElementById("tokensavebtn").disabled = true
-//         document.getElementById("tokensavebtn").style.color = "unset"
-//     }
-// })
-
-
-
-document.getElementById("Token_input").addEventListener("input", () => {
-    var input = document.getElementById("Token_input");
-    var button = document.getElementById("tokensavebtn");
-    var checkbox = document.getElementById("EquipmentCheck");
-    if (input.value.trim() !== "") {
-      button.disabled = false;
-      checkbox.disabled = false;
-      button.style.backgroundColor="#2c6e3a";
-      button.style.cursor="pointer";
-      button.style.color="white";
-      button.style.border = "2px solid white"
-    } else {
-      button.disabled = true;
-      checkbox.disabled = true;
-      button.style.backgroundColor= "#5a8e65";
-      button.style.cursor="default";
-      button.style.border = "2px solid red"
-    }
-})
 
 
 document.querySelector("#quicktoolimg").addEventListener("click", function () {
     window.open("https://www.systems-x.com/en-us/contact-us?hsCtaTracking=e9deae1e-b808-45f9-8eb5-58b99ac20974%7C42c15435-0f3e-4a85-8c8b-d0ab8be4fe67").focus();
 }
 )
-chrome.storage.local.get(["exists"]).then((result) => {
-console.log(result.exists);
-if(result.exists){
-    document.getElementById("isverify").innerHTML = "verify";
-}else{
-    document.getElementById("isverify").innerHTML = "not verify";
-
-}
-})
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//     if (message.type === 'data') {
-//         // Use the data from the message
-//         const data = message.data;
-//         console.log(data);
-//       }
-// })
+chrome.storage.local.get(["TokenValue","pcn"]).then((result) => {
+    pcn = result.pcn
+    authToken = result.TokenValue;
+    fetch('http://localhost:8000/verify-pcn', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          Plexus_Customer_No: pcn,
+          Token: authToken
+        })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          if(data.exists){
+            document.getElementById("isverify").innerHTML = "Verified Token";
+        }else{
+            document.getElementById("isverify").innerHTML = "Invalid Token";
+        
+        }
+          // chrome.runtime.sendMessage({ type: 'data', data: data.exists });
+        })
+        .catch(error => console.error(error));
+  });
