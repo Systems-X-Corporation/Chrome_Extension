aff79aaccd9fb20eb2b6429971ab357e5c541889
sxjugalde <joseph.ugalde@systems-x.com>
2023-06-27 15:26:04 -0600

    updating .env records on db

--- admin-panel/backend/app.js
+++ admin-panel/backend/app.js
@@ -26,7 +26,6 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server:process.env.DB_SERVER,
-  port:process.env.DB_PORT,
   database:process.env.DB_NAME,
   options: {
     encrypt: true // use encrypted connection
